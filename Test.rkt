dfe19623339b805783a70d9e4050f6b170417ca0
vlnn <anohin.va@gmail.com>
2019-11-10 22:15:07 +0200

    Test the failing tests

--- Test.rkt
+++ Test.rkt
@@ -1,8 +1,9 @@
 #lang racket/base
- 
-(require rackunit
-         "Program.rkt")
 
-(check-equal? (my-+ 1 1) 2 "Simple addition")
-(check-equal? (my-* 1 2) 2 "Simple multiplication")
+    (require rackunit
+        "Program.rkt")
+
+    (exit 1)
+    (check-equal? (my-+ 1 1) 2 "Simple addition")
+    (check-equal? (my-* 1 2) 2 "Simple multiplication")
 
---
+++ run-tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+racket Test.rkt 
+
+if [ $? -eq 0 ]
+then
+  echo "Tests ran successfully or didn't ran at all"
+else
+  echo "Tests were failing, life was hard" >&2
+  exit 1
+fi
